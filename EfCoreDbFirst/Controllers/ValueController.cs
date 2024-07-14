301e6deadee4d6fe8bd98a1bf0d6eed049148958
Rohan Sakpal <rohan.sakpal@atriina.com>
2024-07-14 16:16:40 +0530

    First commit

---
+++ EfCoreDbFirst/Controllers/ValueController.cs
@@ -0,0 +1,21 @@
+﻿using EfCoreDbFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreDbFirst.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly minimaldbContext _context;
+        public ValueController(minimaldbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployee()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+    }
+}
---
+++ EfCoreDbFirst/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfCoreDbFirst.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+    }
+}
